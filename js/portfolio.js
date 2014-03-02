d4ddbb822593bb032cf6eb3335a2b1f160027350
da-vid <dave.wong@gmail.com>
2014-03-02 14:16:19 -0800

    Final draft 1

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,4 @@
-(function($) {
+
     jQuery(document).ready(function($) {
         $(".scroll").click(function(event){ // When a link with the .scroll class is clicked
             event.preventDefault(); // Prevent the default action from occurring
@@ -10,49 +10,50 @@
             $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500); // Animate the scroll to this link's href value
         });
 
-         $("#workCarousel").owlCarousel({
+        $('#menu').slicknav();
+
+        $("#workCarousel").owlCarousel({
  
-    // Most important owl features
-    items : 2,
-    itemsDesktop : [1199,2],
-    itemsDesktopSmall : [980,1],
-    itemsTablet: [768,1],
-    itemsTabletSmall: false,
-    itemsMobile : [479,1],
-    singleItem : false,
- 
-     //Autoplay
-    autoPlay : false,
-    stopOnHover : false,
- 
-    // Navigation
-    navigation : false,
-    navigationText : ["Previous","Next"],
- 
-    //Pagination
-    pagination : true,
-    paginationNumbers: false,
- 
-    // Responsive 
-    responsive: true,
-    responsiveRefreshRate : 200,
-    responsiveBaseWidth: window,
- 
-    // CSS Styles
-    baseClass : "owl-carousel",
-    theme : "owl-theme",
- 
-    //Lazy load
-    lazyLoad : false,
-    lazyFollow : true,
-    lazyEffect : "fade",
- 
-    //Auto height
-    autoHeight : false,
- 
-    // Other
-    addClassActive : false,
+            // Most important owl features
+            items : 2,
+            itemsDesktop : [1199,2],
+            itemsDesktopSmall : [980,1],
+            itemsTablet: [768,1],
+            itemsTabletSmall: false,
+            itemsMobile : [479,1],
+            singleItem : false,
+
+            //Autoplay
+            autoPlay : false,
+            stopOnHover : false,
+
+            // Navigation
+            navigation : false,
+            navigationText : ["Previous","Next"],
+
+            //Pagination
+            pagination : true,
+            paginationNumbers: false,
+
+            // Responsive 
+            responsive: true,
+            responsiveRefreshRate : 200,
+            responsiveBaseWidth: window,
+
+            // CSS Styles
+            baseClass : "owl-carousel",
+            theme : "owl-theme",
+
+            //Lazy load
+            lazyLoad : false,
+            lazyFollow : true,
+            lazyEffect : "fade",
+
+            //Auto height
+            autoHeight : false,
+
+            // Other
+            addClassActive : false,
     
-});
+        });
     });
-})(jQuery);
